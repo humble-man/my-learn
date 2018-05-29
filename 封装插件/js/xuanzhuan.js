9a7ce2a2fb15a6f3227082678b23c1271e5e4891
xu <605756936@qq.com>
2018-05-29 11:02:58 +0800

    修改

--- 封装插件/js/xuanzhuan.js
+++
@@ -1,9 +0,0 @@
-
-function xuan(obj, num) {
-	var isSpeed = 2;
-	setInterval(function() {
-		isSpeed += num;
-		console.log(isSpeed);
-		obj.style.transform = 'rotate(' + (isSpeed) + 'deg)';
-	}, 30)
-}
